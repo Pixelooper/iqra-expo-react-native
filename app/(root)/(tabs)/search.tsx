b911df156d52c87cda10ef4e5885e38ecaf2998c
iftikhar rasha <iftikhar.rasha@northsouth.edu>
2025-08-19 18:36:03 +0600

    Normalizing font 0

--- app/(root)/(tabs)/search.tsx
+++ app/(root)/(tabs)/search.tsx
@@ -159,8 +159,8 @@ const Search = () => {
                         />
                         <View className="flex-1 flex items-start justify-between pl-3">
                           <Text className="text-lg leading-8 text-black">
-                              {/* {`সূরা ${item.name_bn}`.normalize('NFC')} */}
-                               {`সূরা ${getSurahNameBn(item.no)}`.normalize('NFC')}
+                              {`সূরা ${item.name_bn}`.normalize('NFC')}
+                               {/* {`সূরা ${getSurahNameBn(item.no)}`.normalize('NFC')} */}
                           </Text>
                           <Text className="text-lg leading-8 text-black">
                             {item.name_ar}
